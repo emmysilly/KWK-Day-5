34347a287151dce77f25d17a7f20743afa5df743
Emmy <EmmyChavez@Apples-MacBook-Pro-47.local>
2019-08-09 11:26:25 -0400

    Update

---
+++ Day-5-WarmUp/Day-5-WarmUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day-5-WarmUp
+//
+//  Created by Emmy on 8/9/19.
+//  Copyright © 2019 Emmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Day5WarmUp.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+//Day 5: Warmup
+//Directions: Create a new folder called Day5 or Day-5 in your KWK folder on your Desktop. Then perform the steps below. Be sure to help a sistah/neighbor out.
+//StoryBoard Practice
+//Step 1: Create a project called Day5Warmup
+//Step 2: Add a Navigation Bar to your View Controller.
+//Step 3: Add a Bar Button to your Navigation Bar
+//Step 4: Add an Image to your View Controller.
+//Step 5: Add text to your View Controller.
+//Step 6: Add an additional View Controller.
+//Step 7: Add a connection (segue) between the two View Controllers.
+//Step 8: (Extension) Create a pathway back to the first View Controller from the second View Controller. Note: we did not do this yesterday; however, you have the skills to make this happen.
+
